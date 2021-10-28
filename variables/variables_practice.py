662615243314fa8755291504a55096f80df10c9d
Dwayne J <89208427+s11383726@users.noreply.github.com>
2021-10-28 12:05:24 -0400

    [7B] v0.1
    added some of the most common code.

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -1,1 +1,11 @@
-#Variable Practice, Dwayne Johnson, 10/28/21 11:39am, version 0.0
+# Variable Practice, Dwayne Johnson, 10/28/21 11:39am, version 0.0
+
+# Variables Types -- most common
+
+my_float = -1.8678291
+my_int = 1.361342
+my_string = "i like trains."
+
+print(my_float)
+print(my_int)
+print(my_string)
